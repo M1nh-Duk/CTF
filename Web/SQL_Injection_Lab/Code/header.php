15eb6f1152759eb04446615e4e85478054e9fbeb
M1nh-Duk <100038173+M1nh-Duk@users.noreply.github.com>
2022-06-30 22:29:16 +0700

    Update header.php

--- Web/SQL_Injection_Lab/Code/header.php
+++ Web/SQL_Injection_Lab/Code/header.php
@@ -1,9 +1,9 @@
-<div class= "header text-center">
+<div class= "header text-center" style="background-color: #ff4757 ;">
             <div class="wrapper" style="border: 0.5px solid white;">
                 <ul>
                     <li style="background-color: #f5f6fa;border:1px solid black;border-radius: 20px;width:5px;"><a href="index.php">Home</a></li>
                     <li style="background-color: #f5f6fa;border:1px solid black;border-radius: 20px;width:5px;"><a href="#">About us</a></li>
-                    <li style="background-color: #f5f6fa;border:1px solid black;border-radius: 20px;width:5px;"><a href="#">Users</a></li>
+                    <li style="background-color: #f5f6fa;border:1px solid black;border-radius: 20px;width:5px;"><a href="user.php">Users</a></li>
                     <li style="background-color: #f5f6fa;border:1px solid black;border-radius: 20px;width:5px;"><a href="login.php">Log out</a></li>
                 </ul>
             </div>
